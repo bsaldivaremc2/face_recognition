d557f5e800ab4d7ff9576ea7d58f1b77cb78c539
bsaldivaremc2 <bsaldivar.emc2@gmail.com>
2019-07-30 16:17:50 +0200

    Fixed tutorial

--- create_db/Facenet/flask_app/proc.py
+++
@@ -1,7 +0,0 @@
-from vars import *
-from libs import *
-
-
-REPRESENTATIONS = load_obj(REPRESENTATIONS)
-FOUND_FACES = load_obj(FOUND_FACES)
-load_image_and_save(INPUT_IMG,REPRESENTATIONS,FOUND_FACES)
